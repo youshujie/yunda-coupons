d111e7e6f6c2a0f15eff6b186920cd3469c6e9d0
tobacco91 <991243902@qq.com>
2017-03-31 22:35:27 +0800

    然而js文件合并来不及了

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,4 @@
-"use strict";
-function $ (selector) {
-    if (document.querySelectorAll(selector).length === 1) {
-        return document.querySelector(selector);
-    } else {
-        return document.querySelectorAll(selector);
-    }
-};
 
-var ajax = function (conf) {
-    var method = conf.method;
-    var url = conf.url;
-    var success = conf.success;
-    var data = conf.data;
-    var xhr = new XMLHttpRequest();
-
-    xhr.open(method, url, true);
-
-    if (method == 'GET' || method == 'get') {
-        xhr.send(null);
-    } else if (method == 'POST' || method == 'post') {
-        xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-        xhr.send(data);
-    }
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            success(JSON.parse(xhr.responseText));
-        }
-    };
-    
-};
 $(".card").addEventListener('click', () => {
     //有坑 会动态改变 要在改变之前写入容器
     let length = $('.cards').length;
